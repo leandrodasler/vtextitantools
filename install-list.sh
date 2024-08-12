a50072a85f54fb4ae3abda7d788c37d05ad3dbf3
Tiago de Andrade Freire <tiago.freire@cubos.io>
2024-08-12 17:13:20 -0300

    feat: improving product comparison and discount image badge

--- install-list.sh
+++ install-list.sh
@@ -1,33 +1,32 @@
 #!/bin/bash
-set -e
 
-vtex install vtex.reviews-and-ratings@3.x
-vtex install vtex.admin@4.x
-vtex install vtex.admin-customers@2.x
-vtex install vtex.admin-home@3.x
-vtex install vtex.b2b-checkout-settings@1.x
-vtex install vtex.b2b-orders-history@0.x
-vtex install vtex.b2b-organizations@1.x
-vtex install vtex.b2b-quotes@1.x
-vtex install vtex.checkout-ui-custom@0.x
-vtex install vtex.my-account@1.x
-vtex install vtex.quickorder@3.x
-vtex install vtex.storefront-permissions-ui@1.x
-vtex install vtex.wish-list@1.x
-vtex install vtex.location-availability@0.x
-vtex install vtex.shopper-location@0.x
-vtex install vtex.wordpress-integration@2.x
-vtex install vtex.search-resolver@1.x
-vtex install vtex.admin-graphql-ide@3.x
-vtex install vtex.admin-promotions@1.x
-vtex install vtex.admin-search@1.x
-vtex install vtex.admin-webpage-performance@0.x
-vtex install vtex.customer-credit@4.x
-vtex install vtex.my-credits@1.x
-vtex install vtex.pagespeed-graphql@0.x
-vtex install vtex.social-selling@0.x
-vtex install vtex.storefront-permissions@1.x
-vtex install vtex.telemarketing@2.x
-vtex install ssesandbox04.threekit-app@0.x
-vtex install ssesandbox04.seller-page@0.x
-vtex install ssesandbox04.b2b-suite-bra@1.x
+vtex install vtex.reviews-and-ratings@3.x \
+    vtex.admin@4.x \
+    vtex.admin-customers@2.x \
+    vtex.admin-home@3.x \
+    vtex.b2b-checkout-settings@1.x \
+    vtex.b2b-orders-history@0.x \
+    vtex.b2b-organizations@1.x \
+    vtex.b2b-quotes@1.x \
+    vtex.checkout-ui-custom@0.x \
+    vtex.my-account@1.x \
+    vtex.quickorder@3.x \
+    vtex.storefront-permissions-ui@1.x \
+    vtex.wish-list@1.x \
+    vtex.location-availability@0.x \
+    vtex.shopper-location@0.x \
+    vtex.wordpress-integration@2.x \
+    vtex.search-resolver@1.x \
+    vtex.admin-graphql-ide@3.x \
+    vtex.admin-promotions@1.x \
+    vtex.admin-search@1.x \
+    vtex.admin-webpage-performance@0.x \
+    vtex.customer-credit@4.x \
+    vtex.my-credits@1.x \
+    vtex.pagespeed-graphql@0.x \
+    vtex.social-selling@0.x \
+    vtex.storefront-permissions@1.x \
+    vtex.telemarketing@2.x \
+    ssesandbox04.threekit-app@0.x \
+    ssesandbox04.seller-page@0.x \
+    ssesandbox04.b2b-suite-bra@1.x
